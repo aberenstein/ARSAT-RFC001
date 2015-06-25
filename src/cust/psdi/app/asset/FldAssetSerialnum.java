7c91c005e1a9d77d78decd1ec11826c4403c1d54
abi <abel.berenstein@priux.com.ar>
2015-06-25 09:27:02 -0300

    Correccion de bugs.

--- src/cust/psdi/app/asset/FldAssetSerialnum.java
+++ src/cust/psdi/app/asset/FldAssetSerialnum.java
@@ -1,17 +1,12 @@
 package cust.psdi.app.asset;
 
-import java.rmi.RemoteException;
-
-import psdi.app.asset.Asset;
-import psdi.mbo.Mbo;
 import psdi.mbo.MboValue;
-import psdi.tamit.app.asset.TAMITFldAssetChild;
+import psdi.mbo.MboValueAdapter;
 import psdi.util.MXApplicationException;
 import psdi.util.MXException;
 
-public class FldAssetSerialnum extends TAMITFldAssetChild {
-	String chipsetnum;
-	int fldWidth = 12;
+public class FldAssetSerialnum extends MboValueAdapter {
+	final int fldWidth = 12;
 	
 	public FldAssetSerialnum(MboValue mbv) throws MXException {
 		super(mbv);
@@ -24,23 +19,15 @@ public class FldAssetSerialnum extends TAMITFldAssetChild {
 		if(fldValue.contains("|"))
 		{
 			int pipeIndex = fldValue.indexOf("|");
-			chipsetnum = fldValue.substring(pipeIndex + 1); 
-			String serialnum = fldValue.substring(0, pipeIndex - 1).trim().substring(0, fldWidth);
 
-		    Asset asset = (Asset)getMboValue().getMbo();
-		    asset.setValue("serialnum", serialnum, Mbo.NOVALIDATION);
+			String serialnum = fldValue.substring(0, pipeIndex - 1);
+			getMboValue("SERIALNUM").setValue(serialnum, NOACCESSCHECK|NOVALIDATION_AND_NOACTION);
+
+			String chipsetnum = fldValue.substring(pipeIndex + 1);
+			getMboValue("ARS_CHIPSETNUM").setValue(chipsetnum, NOACCESSCHECK|NOVALIDATION_AND_NOACTION);
 		}
 		
 		super.validate();
     }
 	
-	public void action() throws MXException, RemoteException
-    {
-		if (!chipsetnum.isEmpty())
-		{
-		    Asset asset = (Asset)getMboValue().getMbo();
-		    asset.setValue("chipsetnum", chipsetnum, Mbo.NOACCESSCHECK);
-		}
-	}
-			  
 }
